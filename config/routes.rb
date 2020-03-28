14b2ab5c922461eb038225d8f229b604ce5fb9ca
Shoriii <cerezos1206@gmail.com>
2020-03-28 06:29:41 +0000

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
     registrations: 'admins/registrations'
   }
   devise_for :customers, controllers: {
-    sessions:      'customers/sessions',
-    passwords:     'customers/passwords',
-    registrations: 'customers/registrations'
+    sessions:      'customers_devise/sessions',
+    passwords:     'customers_devise/passwords',
+    registrations: 'customers_devise/registrations'
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
